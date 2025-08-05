c042e34118e1b7da8d1bede25d099709e7f2f26b
zhanat-B-skaeng <zhanat07021986@gmail.com>
2025-08-05 20:03:25 +0300

    проверить ДЗ-6

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,103 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Conditional operator");
+        System.out.println("Conditional operator");
+        System.out.println("Задача 1");
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+        System.out.println("Задача 2");
+
+        int temperature = 3;
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+
+        }
+
+        System.out.println("Задача 3");
+
+        int speed = 90;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + ", придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + ", можно ездить спокойно");
+
+
+        }
+        System.out.println("Задача 4");
+
+        age = 40;
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+        }
+        if (age >= 7 && age <= 17) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
+        }
+        if (age >= 18 && age <= 24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в университет");
+        }
+        if (age > 24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу");
+        }
+
+        System.out.println("Задача 5");
+
+        age = 12;
+        if (age < 5) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе");
+        }
+        if (age >= 5 && age < 14) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе в сопровождении");
+        }
+        if (age >= 14) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе без сопровождения взрослого");
+        }
+
+        System.out.println("Задача 6");
+
+        int places = 102;
+        int sitPlaces = 60;
+        int people = 75;
+
+        if (people < sitPlaces) {
+            System.out.println("В вагоне есть сидячие места");
+        }
+        if (people >= sitPlaces && people < places) {
+            System.out.println("В вагоне есть стоячие места");
+        }
+        if (people >= places) {
+            System.out.println("В вагоне нет мест");
+        }
+
+        System.out.println("Задача 7");
+
+        int one = 90;
+        int two = 7;
+        int three = 35;
+
+        if (one >= two && one >= three) {
+            System.out.println("Максимальное число в переменной one, равно " + one);
+        } else {
+            if (two >= three) {
+                System.out.println("Максимальное число в переменной two, равно " + two);
+            } else {
+                System.out.println("Максимальное число в переменной three, равно " + three);
+
+            }
+
+        }
+
+
+
+
+
+
+
     }
 }
